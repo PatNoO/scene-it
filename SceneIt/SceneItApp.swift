79ab64ee7884e05472efe5ff77271560c897a54a
PatNoO <Noordh94@hotmail.se>
2026-03-30 11:48:22 +0200

    Initial Commit

---
+++ SceneIt/SceneItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneItApp.swift
+//  SceneIt
+//
+//  Created by Patrik Noordh on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct SceneItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
