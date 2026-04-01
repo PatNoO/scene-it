e4315c429807bd2a64cb744a5bc733991fec4908
Linnéa <linnea.ternevik@gmail.com>
2026-04-01 19:09:56 +0200

    refactor: updated AppViewModel and SceneItApp routing

--- SceneIt/SceneItApp.swift
+++ SceneIt/SceneItApp.swift
@@ -1,26 +1,21 @@
 import SwiftUI
-// NOTE: This file contains intentional compiler errors.
-// Waiting for StartViewModel and QuizViewModel to be updated
-// with onStart and onFinished callbacks before this compiles.
+
 @main
 struct SceneItApp: App {
- 
+
     @StateObject private var appViewModel = AppViewModel()
- 
+
     var body: some Scene {
         WindowGroup {
             switch appViewModel.screen {
             case .start:
-                StartView(state: StartViewModel(onStart: appViewModel.startQuiz).state)
- 
-            case .quiz(let category):
-                QuizView(
-                    viewModel: QuizViewModel(
-                        category: category,
-                        onFinished: appViewModel.showResult
-                    )
-                )
- 
+                StartView(state: appViewModel.startViewModel.state)
+
+            case .quiz:
+                if let vm = appViewModel.quizViewModel {
+                    QuizView(state: vm.state)
+                }
+
             case .result(let score, let total, let category):
                 ResultView(
                     state: ResultViewModel(
